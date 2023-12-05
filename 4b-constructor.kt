231ffeef18eced017b1a447bac7f880878240b44
go-kul-anand <152949469+go-kul-anand@users.noreply.github.com>
2023-12-05 10:58:01 +0530

    Create 4b-constructor.kt

---
+++ 4b-constructor.kt
@@ -0,0 +1,8 @@
+class Rectangle(length : Int, breadth: Int){ val 1 : Int val b : Int init{
+1 = length b = breadth
+println("Area of Rectangle is ${l*b}")
+}}
+fun main(args: Array<String>) { Rectangle(4, 5)
+Rectangle(5, 8)
+}
+
