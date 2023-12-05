0351bd68e4dc5caf354d30a81e892bc1c6768f68
go-kul-anand <152949469+go-kul-anand@users.noreply.github.com>
2023-12-05 10:55:38 +0530

    Create 3-freq char in string.kt

---
+++ 3-freq char in string.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) { val str = "I am happy"
+val ch= 'm'
+var frequency = 0
+for (i in O..str.length - 1) { if (ch== str[i]) {
+++frequency
+}}
+println("Frequency of $ch = $frequency")
+}
