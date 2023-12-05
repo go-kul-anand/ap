5c55074f27167d4a44640796667ede45a0c44142
go-kul-anand <152949469+go-kul-anand@users.noreply.github.com>
2023-12-05 10:52:34 +0530

    Create 1a-biodata.kt

---
+++ 1a-biodata.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>)
+{
+println("Enter Name: ")
+ 
+
+val name = readLine() println("Enter Age: ")
+var age : Int= Integer.parselnt(readLine()) println("Job: ")
+val job = readLine()
+println("This is $name . He is $age years old. He is a $job .")}
+
